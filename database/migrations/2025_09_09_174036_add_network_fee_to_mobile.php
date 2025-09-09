7622fde2e7672b8d23c62636589ef64bc357fae4
chandachewe10 <chewec03@gmail.com>
2025-09-09 10:43:09 -0700

    chore: added migrations for total sats and network fee

---
+++ database/migrations/2025_09_09_174036_add_network_fee_to_mobile.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bit_coin_to_mobile_money', function (Blueprint $table) {
+            $table->decimal('total_sats', 16, 8)->nullable();
+            $table->decimal('network_fee', 16, 8)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bit_coin_to_mobile_money', function (Blueprint $table) {
+            //
+        });
+    }
+};
