c75712db1bc6da5120f42e0e29fb21de21cd06c7
unknown <chewec03@gmail.com>
2025-10-23 01:13:28 +0200

    chore: can access pannel in production

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Filament\Panel;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
@@ -47,8 +48,8 @@ class User extends Authenticatable
     }
 
 
-     public function canAccessFilament(): bool
+public function canAccessPanel(Panel $panel): bool
     {
-        return true; 
+        return true;
     }
 }
