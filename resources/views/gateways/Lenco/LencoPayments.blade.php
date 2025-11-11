9660b33b90621b7494e3c2525fa31664fbf3524f
chandachewe10 <chewec03@gmail.com>
2025-11-10 22:23:40 -0800

    fix: fix typo

--- resources/views/gateways/Lenco/LencoPayments.blade.php
+++ resources/views/gateways/Lenco/LencoPayments.blade.php
@@ -38,7 +38,7 @@
                     .then(res => res.json())
                     .then(data => {
                         if (data.status === "success") {
-                            alert("✅ Bitcoin withdrawal created successfully! Scan the QR code using your walletto receive the payment.");
+                            alert("✅ Bitcoin withdrawal created successfully! Scan the QR code using your wallet to receive the payment.");
 
                             // If QR code is returned, show it
                             window.location.href = "/customer/mobile-to-bitcoins";
